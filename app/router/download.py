33739e8361ed80f66e9aa36ab43e8a45fcbc7cbb
[emilla6535] <[milla_e_6535@yahoo.com]>
2025-01-24 00:00:04 -0300

    Get GPs and drivers from OpenF1

--- app/router/download.py
+++ app/router/download.py
@@ -1,4 +1,5 @@
 from app import templates, sections
+from app.utils.utils import loadDataFromUrl
 from fastapi import APIRouter, Request
 from fastapi.staticfiles import StaticFiles
 
@@ -23,23 +24,46 @@ async def get_main_download(request: Request):
 @router.get(path="/gp-select")
 async def get_download_gp(request: Request, year: int):
     # get a list of drivers acronyms from the GP
-    if year == 2024:
-        gps = {'formula-1-bahrain-grand-prix-2024': "FORMULA 1 BAHRAIN GRAND PRIX 2024",
-               'formula-1-stc-saudi-arabian-grand-prix-2024': "FORMULA 1 STC SAUDI ARABIAN GRAND PRIX 2024"}
-    else:
-        gps = {'formula-1-lenovo-chinese-grand-prix-2023': "FORMULA 1 LENOVO CHINESE GRAND PRIX 2023",
-               'formula-1-heineken-0-las-vegas-grand-prix-2023': "FORMULA 1 HEINEKEN 0 LAS VEGAS GRAND PRIX 2023"}
+    #if year == 2024:
+    #    gps = {'formula-1-bahrain-grand-prix-2024': "FORMULA 1 BAHRAIN GRAND PRIX 2024",
+    #           'formula-1-stc-saudi-arabian-grand-prix-2024': "FORMULA 1 STC SAUDI ARABIAN GRAND PRIX 2024"}
+    #else:
+    #    gps = {'formula-1-lenovo-chinese-grand-prix-2023': "FORMULA 1 LENOVO CHINESE GRAND PRIX 2023",
+    #           'formula-1-heineken-0-las-vegas-grand-prix-2023': "FORMULA 1 HEINEKEN 0 LAS VEGAS GRAND PRIX 2023"}
+    
+    meeting_url = "https://api.openf1.org/v1/meetings?year=" + str(year)
+    year_meeting = loadDataFromUrl(meeting_url)
+    gps = {item['meeting_key']: item['meeting_official_name'] for item in year_meeting}
+    
     context["request"] = request
     context["gps"] = gps
     
     return templates.TemplateResponse("partials/download_partials/gp_selector.html", context)
 
 @router.get(path="/driver-select")
-async def get_download_drivers(request: Request, grandprix: str):
+async def get_download_drivers(request: Request, meetingkey: int):
     # get all the drivers found in the gp
     # maybe use a dict {'ACRONYM': number}
-    drivers = ["VER", "PER", "SAI", "LEC"]
+    drivers_url = "https://api.openf1.org/v1/drivers?meeting_key=" + str(meetingkey)
+    drivers_in_meeting = loadDataFromUrl(drivers_url)
+    drivers = []
+    for item in drivers_in_meeting:
+        driver_acronym = item['name_acronym']
+        if driver_acronym not in drivers:
+            drivers.append(item['name_acronym'])
+            #drivers[driver_acronym] = item['driver_number']
+
+    #drivers = ["VER", "PER", "SAI", "LEC"]
     context["request"] = request
     context["drivers"] = drivers
 
     return templates.TemplateResponse("partials/download_partials/driver_selector.html", context)
+
+@router.post(path="/download-data")
+async def download_data(request: Request): # <- receive parameters
+    # send status messages of the downloads
+    # loop over all requested drivers
+    # download the data one by one
+    # yield the message
+    all_messages = ["Message 1", "Message 2", "Message 3", "Message 4"]
+    
---
+++ app/utils/utils.py
@@ -0,0 +1,56 @@
+import urllib
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import json
+import os
+import time
+
+def downloadFile(download_url, filename, retries=0):
+    try:
+        response = urlopen(download_url)
+        data_json = json.loads(response.read())
+        with open(filename, "w") as outfile:
+            json.dump(data_json, outfile)
+    except HTTPError as e:
+        content = e.read()
+        if retries < 4:
+            print("Error downloading the data. Waiting 60 seconds.")
+            time.sleep(60)
+            downloadFile(download_url, filename, retries+1)
+        else:
+            print("Maximum retries. Check internet connection or file availability.")
+
+def loadDataFromUrl(download_url, retries=0):
+    try:
+        response = urlopen(download_url)
+        data_json = json.loads(response.read())
+    except HTTPError as e:
+        content = e.read()
+        if retries < 4:
+            print("Error downloading the data. Waiting 60 seconds.")
+            time.sleep(60)
+            data_json = loadDataFromUrl(download_url, retries+1)
+        else:
+            print("Maximum retries. Check internet connection or file availability.")
+            data_json = {}
+    
+    return data_json
+
+def loadDataFromDisk(filepath):
+    with open(filepath, 'r') as file:
+        data_json = json.load(file)
+    return data_json
+
+def updateFile(url, filename, check_for_update=True):
+    wait = False
+    if not os.path.exists(filename):
+        downloadFile(url, filename)
+        time.sleep(5)
+    elif check_for_update:
+        # Check for updates
+        offline_data = loadDataFromDisk(filename)
+        online_data = loadDataFromUrl(url)
+        if len(online_data) > len(offline_data):
+            with open(filename, 'w') as outfile:
+                json.dump(online_data, outfile)
+            time.sleep(5)
